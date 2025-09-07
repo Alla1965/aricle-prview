f534de122274f39f215d51c6c01f1cc189862496
Alla1965 <savanna.opt@gmail.com>
2025-09-07 20:49:08 +0300

    second time

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 const btnShare = document.querySelector(".btn-share");
 const socialLinks = document.querySelector(".social-links");
-console.log(btnShare);
-console.log(socialLinks);
 
 btnShare.addEventListener("click", () => {
   if (
     socialLinks.style.display === "none" ||
     socialLinks.style.display === ""
   ) {
-    // socialLinks.style.display = "block";
     socialLinks.classList.toggle("show");
     console.log(socialLinks.classList);
   } else {
